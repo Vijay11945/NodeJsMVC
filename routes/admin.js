1caf9850ec4e138ef72c3a9cf4ea029cf81780d9
Vijay11945 <120110419+Vijay11945@users.noreply.github.com>
2023-08-18 19:34:18 +0530

    Add files via upload

--- routes/admin.js
+++ routes/admin.js
@@ -8,10 +8,10 @@ const router = express.Router();
 
 
 // /admin/add-product => GET
-router.get('/add-product', productsControlle.getAddProduct);
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productsControlle.postAddProduct );
+router.post('/add-product', productsController.postAddProduct );
 
 //exports.routes = router;
 //exports.products = products;
